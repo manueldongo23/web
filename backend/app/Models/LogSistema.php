f8421e9fe5e9a66047df18ce804d287390f48717
MANUEL_DONGO <md2023076842@virtual.upt.pe>
2026-05-03 22:50:36 -0500

    Delete backend directory

--- backend/app/Models/LogSistema.php
+++
@@ -1,8 +0,0 @@
-<?php
-class LogSistema {
-    public static function registrar($tipo, $mensaje, $userId = null) {
-        $db = Database::getConnection();
-        $stmt = $db->prepare("INSERT INTO logs_sistema (tipo, mensaje, user_id) VALUES (?, ?, ?)");
-        $stmt->execute([$tipo, $mensaje, $userId]);
-    }
-}
